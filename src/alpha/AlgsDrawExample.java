4bd3ccd4d54546f6609ff838e62791cfc8aa41ba
zhaozigu <zhaozigunan@163.com>
2020-11-26 00:15:26 +0800

    添加《算法》第四版依赖

---
+++ src/alpha/AlgsDrawExample.java
@@ -0,0 +1,18 @@
+package alpha;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+class AlgsDrawExample {
+  public static void main(String[] args) {
+    int N = 100;
+    StdDraw.setXscale(0, N);
+    StdDraw.setYscale(0, N * N);
+    StdDraw.setPenRadius(.01);
+
+    for (int i = 1; i <= N; i++) {
+      StdDraw.point(i, i);
+      StdDraw.point(i, i * i);
+      StdDraw.point(i, i * Math.log(i));
+    }
+  }
+}
