25e526ce50f1e2dde2574ae2946d3d86151485b4
zhaozigu <zhaozigunan@163.com>
2020-12-09 21:28:12 +0800

    新增 Leetcode202 题解

---
+++ src/leetcode/leetcode202_happy_number.cc
@@ -0,0 +1,33 @@
+// https://leetcode-cn.com/problems/happy-number/
+class Solution
+{
+public:
+  bool isHappy(int n)
+  {
+    int cnt = 0;
+    while (n != 1)
+    {
+      cnt++;
+      int temp = 0;
+      while (n)
+      {
+        temp = temp + (n % 10) * (n % 10);
+        n = n / 10;
+      }
+      n = temp;
+      if (cnt == 10)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+#include "gtest/gtest.h"
+
+TEST(leetcode202, sampleInputByProblem)
+{
+  Solution solution;
+  ASSERT_TRUE(solution.isHappy(19));
+}
