14e3eb8f191b21262380940f05571da0387ca64f
zhaozigu <zhaozigunan@163.com>
2021-12-03 23:40:22 +0800

    fixed

--- src/onlinejudge/poj1003_test.cc
+++ src/onlinejudge/poj1003_test.cc
@@ -7,7 +7,7 @@ TEST(testPoj1003, sampleInputByProblem)
   double ins[] = {1.00, 3.71, 0.04, 5.19, NAN};
   int ex_out[4] = {3, 61, 1, 273};
   Solve solve;
-  for (int i = 0; !isnan(ins[i]); i++)
+  for (int i = 0; !std::isnan(ins[i]); i++)
   {
     ASSERT_EQ(ex_out[i], solve.GetCardNum(ins[i]));
   }
